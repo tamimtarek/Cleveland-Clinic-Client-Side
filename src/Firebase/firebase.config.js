099d64f342b6ad84c225cb7cddfb5cd05326f7ef
tamimtarik <tamimtarik15@gmail.com>
2024-12-16 22:56:51 +0600

    update node module

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  apiKey: "AIzaSyACpnnvpVv7HFq3WLPNdCKHYRLq4xCK4eg",
+  authDomain: "cleveland-c02d4.firebaseapp.com",
+  projectId: "cleveland-c02d4",
+  storageBucket: "cleveland-c02d4.firebasestorage.app",
+  messagingSenderId: "679806361784",
+  appId: "1:679806361784:web:cadb928c651273df82144d"
 };
 
 // Initialize Firebase
